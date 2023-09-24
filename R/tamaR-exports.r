709835e4d1f53ad5e2d3341665fa69a5931e67be
Almarch <alexandre.marchal@hotmail.com>
2023-09-24 13:27:50 +0200

    integrate prepare_img to tamaR-exports

--- R/tamaR-exports.r
+++ R/tamaR-exports.r
@@ -18,6 +18,13 @@
 #' 
 #' 
 
+bg = readPNG("img/background.png")
+icons = list()
+for(ics in c("attention","bathroom","food","game",
+             "lights","medicine","status","training")){
+    icons[[ics]] = readPNG(paste0("img/",ics,".png"))
+}
+
 setRcppClass(Class = "Tama",
              CppClass = "Tama",
              module = "Tamalib",
