03b3e241579a2723f6ac44ae6f4dc53ec954fb1c
almarch <alexandre.marchal@hotmail.com>
2024-04-09 20:01:27 +0200

    (indent)

--- R/go.R
+++ R/go.R
@@ -390,7 +390,7 @@ go = function(tama, background = NULL, port = 1996, host = "127.0.0.1"){
             })
         })
 
-         output$save_rom <- downloadHandler(
+        output$save_rom <- downloadHandler(
             filename = "rom.h",
             content = function(file) {
                 tama$stop()
@@ -421,161 +421,161 @@ go = function(tama, background = NULL, port = 1996, host = "127.0.0.1"){
 
         ## care routine
         observe({
-        if(!etc[["busy"]]) {
-            etc[["busy"]] = T
+            if(!etc[["busy"]]) {
+                etc[["busy"]] = T
 
-            t1 = Sys.time()
-            elapsed = as.numeric(difftime(t1,etc[["t0"]],units = "sec"))
-            etc[["t0"]] = t1
+                t1 = Sys.time()
+                elapsed = as.numeric(difftime(t1,etc[["t0"]],units = "sec"))
+                etc[["t0"]] = t1
 
-            if(input$care){
+                if(input$care){
 
-                ### if it's an egg, set the clock
-                if(etc[["egg"]]) {
-                    if(is.egg(tama)) {
-                        etc[["todo"]] = set_clock()
+                    ### if it's an egg, set the clock
+                    if(etc[["egg"]]) {
+                        if(is.egg(tama)) {
+                            etc[["todo"]] = set_clock()
+                        }
+                        etc[["egg"]]  = F
                     }
-                    etc[["egg"]]  = F
-                }
 
-                ### if dead, that's over
-                if(is.dead(tama)) {
-                    etc[["dead"]] = T
-                    etc[["todo"]]$wait = Inf
-                }
-                
-                ### stop the need to scold if it doesn't cry anymore
-                if(!tama$GetIcon()[8]) {
-                    etc[["scold"]] = F
-                }
-                
-                ### otherwise, plan an action
-                if(is.asleep(tama,"off")){
-                    etc[["stats"]] = c(hunger = 4,
-                                    happiness = 4)
+                    ### if dead, that's over
+                    if(is.dead(tama)) {
+                        etc[["dead"]] = T
+                        etc[["todo"]]$wait = Inf
+                    }
+                    
+                    ### stop the need to scold if it doesn't cry anymore
+                    if(!tama$GetIcon()[8]) {
+                        etc[["scold"]] = F
+                    }
+                    
+                    ### otherwise, plan an action
+                    if(is.asleep(tama,"off")){
+                        etc[["stats"]] = c(hunger = 4,
+                                        happiness = 4)
 
-                } else if(etc[["todo"]]$wait <= 0 &
-                        length(etc[["todo"]]$actions) == 0) {
+                    } else if(etc[["todo"]]$wait <= 0 &
+                            length(etc[["todo"]]$actions) == 0) {
 
-                    # end what has been started
-                    if(etc[["doing"]] != "") {
-                        if(etc[["doing"]] == "try_to_clean") {
-                            if(is.dirty(tama,"top")) {
-                                # still dirty => asleep, turn the light off
-                                etc[["todo"]] = light()
+                        # end what has been started
+                        if(etc[["doing"]] != "") {
+                            if(etc[["doing"]] == "try_to_clean") {
+                                if(is.dirty(tama,"top")) {
+                                    # still dirty => asleep, turn the light off
+                                    etc[["todo"]] = light()
+                                    etc[["doing"]] = ""
+                                }
+
+                            } else if(etc[["doing"]] == "check_arrow") {
+                                # now that the arrow is checked, feed
+                                etc[["todo"]] = feed(ifelse(is.burger(tama),
+                                                "top",
+                                                "bottom"),
+                                            times = 4 - etc[["stats"]]["hunger"])
+                                etc[["stats"]]["hunger"] = 4
                                 etc[["doing"]] = ""
+
+                            } else if(etc[["doing"]] == "check_status_1") {
+                                # check hunger
+                                etc[["stats"]]["hunger"] = nb.hearts(tama)
+                                etc[["todo"]] = check_status(step = 2)
+                                etc[["doing"]] = "check_status_2"
+
+                            } else if(etc[["doing"]] == "check_status_2") {
+                                # check happiness
+                                etc[["stats"]]["happiness"] = nb.hearts(tama)
+                                etc[["todo"]] = check_status(step = 3)
+                                etc[["doing"]] = ""
+
+                                if(tama$GetIcon()[8] &
+                                !is.asleep(tama, "on") &
+                                etc[["stats"]]["hunger"] > 0 &
+                                etc[["stats"]]["happiness"] > 0){
+                                    etc[["scold"]] = T
+                                }
                             }
 
-                        } else if(etc[["doing"]] == "check_arrow") {
-                            # now that the arrow is checked, feed
-                            etc[["todo"]] = feed(ifelse(is.burger(tama),
-                                            "top",
-                                            "bottom"),
-                                        times = 4 - etc[["stats"]]["hunger"])
-                            etc[["stats"]]["hunger"] = 4
-                            etc[["doing"]] = ""
+                        # check bad screens (clock, light off when not asleep)
+                        } else if(is.clock(tama)) {
+                            etc[["todo"]] = unclock()
 
-                        } else if(etc[["doing"]] == "check_status_1") {
-                            # check hunger
-                            etc[["stats"]]["hunger"] = nb.hearts(tama)
-                            etc[["todo"]] = check_status(step = 2)
-                            etc[["doing"]] = "check_status_2"
+                        } else if(is.dark(tama)) {
+                            etc[["todo"]] = light()
 
-                        } else if(etc[["doing"]] == "check_status_2") {
-                            # check happiness
-                            etc[["stats"]]["happiness"] = nb.hearts(tama)
-                            etc[["todo"]] = check_status(step = 3)
-                            etc[["doing"]] = ""
+                        # cares
+                        } else if(is.asleep(tama,"on")) {
+                            etc[["todo"]] = light()
 
-                            if(tama$GetIcon()[8] &
-                            !is.asleep(tama, "on") &
-                            etc[["stats"]]["hunger"] > 0 &
-                            etc[["stats"]]["happiness"] > 0){
-                                etc[["scold"]] = T
-                            }
+                        } else if(is.dirty(tama,"top")) {
+                            # double poop: try to clean - or is it asleep ?
+                            etc[["todo"]] = clean()
+                            etc[["doing"]] = "try_to_clean"
+
+                        } else if(is.dirty(tama,"bottom")) {
+                            etc[["todo"]] = clean()
+
+                        } else if(is.sick(tama)) {
+                            # heal after double poop that may hide it
+                            etc[["todo"]] = heal()
+
+                        } else if(input$disc & etc[["scold"]]) {
+                            etc[["todo"]] = scold()
+                            etc[["scold"]] = F
+
+                        } else if(etc[["stats"]]["hunger"] < 4 & !etc[["scold"]]){
+                            etc[["todo"]] = check_food_arrow()
+                            etc[["doing"]] = "check_arrow"
+
+                        } else if(etc[["stats"]]["happiness"] < 4 & !etc[["scold"]]){
+
+                            etc[["todo"]] = play_game(times = min(2,4 - etc[["stats"]]["happiness"]))
+                            etc[["stats"]]["happiness"] = 4
+                            etc[["next_check"]] = 0
+
+                            # if it's time and it's not sleeping,
+                            # or if it's crying with no apparent reason
+                            # reasons: light on when sleeping, hungry, unhappy, to scold
+                            # hungry and unhappy must be checked, if checked and !=0: "to scold"
+                        } else if(etc[["t0"]] > etc[["next_check"]] |
+                                        (tama$GetIcon()[8] &
+                                        !is.asleep(tama,"on"))) {
+                                etc[["next_check"]] = etc[["t0"]] + 5*60
+                                etc[["todo"]] = check_status(step = 1)
+                                etc[["doing"]] = "check_status_1"
+                                etc[["scold"]] = F # we will check that
+                        }
+                    }
+                }
+
+                ### do what has been planned
+                if(etc[["todo"]]$wait > 0) {
+                    etc[["todo"]]$wait = etc[["todo"]]$wait - elapsed
+                } else if(etc[["todo"]]$unclick){
+                    for(b in 0:2) tama$SetButton(b,F)
+                    etc[["todo"]]$wait =  .2
+                    etc[["todo"]]$unclick =  F
+                } else {
+                    if(length(etc[["todo"]]$actions) > 0){
+
+                        act = etc[["todo"]]$actions[1]
+
+                        if(act %in% c("A","B","C")) {
+                            tama$SetButton(c(A = 0, B = 1, C = 2)[act],T)
+                            etc[["todo"]]$wait =  .1
+                            etc[["todo"]]$unclick =  T
+                        } else if(act == "AC") {
+                            for(b in c(0,2)) tama$SetButton(b,T)
+                            etc[["todo"]]$wait =  2
+                            etc[["todo"]]$unclick =  T
+                        } else {
+                            etc[["todo"]]$wait = as.numeric(act)
                         }
 
-                    # check bad screens (clock, light off when not asleep)
-                    } else if(is.clock(tama)) {
-                        etc[["todo"]] = unclock()
-
-                    } else if(is.dark(tama)) {
-                        etc[["todo"]] = light()
-
-                    # cares
-                    } else if(is.asleep(tama,"on")) {
-                        etc[["todo"]] = light()
-
-                    } else if(is.dirty(tama,"top")) {
-                        # double poop: try to clean - or is it asleep ?
-                        etc[["todo"]] = clean()
-                        etc[["doing"]] = "try_to_clean"
-
-                    } else if(is.dirty(tama,"bottom")) {
-                        etc[["todo"]] = clean()
-
-                    } else if(is.sick(tama)) {
-                        # heal after double poop that may hide it
-                        etc[["todo"]] = heal()
-
-                    } else if(input$disc & etc[["scold"]]) {
-                        etc[["todo"]] = scold()
-                        etc[["scold"]] = F
-
-                    } else if(etc[["stats"]]["hunger"] < 4 & !etc[["scold"]]){
-                        etc[["todo"]] = check_food_arrow()
-                        etc[["doing"]] = "check_arrow"
-
-                    } else if(etc[["stats"]]["happiness"] < 4 & !etc[["scold"]]){
-
-                        etc[["todo"]] = play_game(times = min(2,4 - etc[["stats"]]["happiness"]))
-                        etc[["stats"]]["happiness"] = 4
-                        etc[["next_check"]] = 0
-
-                        # if it's time and it's not sleeping,
-                        # or if it's crying with no apparent reason
-                        # reasons: light on when sleeping, hungry, unhappy, to scold
-                        # hungry and unhappy must be checked, if checked and !=0: "to scold"
-                    } else if(etc[["t0"]] > etc[["next_check"]] |
-                                    (tama$GetIcon()[8] &
-                                    !is.asleep(tama,"on"))) {
-                            etc[["next_check"]] = etc[["t0"]] + 5*60
-                            etc[["todo"]] = check_status(step = 1)
-                            etc[["doing"]] = "check_status_1"
-                            etc[["scold"]] = F # we will check that
+                        etc[["todo"]]$actions = etc[["todo"]]$actions[-1]
                     }
                 }
             }
-
-            ### do what has been planned
-            if(etc[["todo"]]$wait > 0) {
-                etc[["todo"]]$wait = etc[["todo"]]$wait - elapsed
-            } else if(etc[["todo"]]$unclick){
-                for(b in 0:2) tama$SetButton(b,F)
-                etc[["todo"]]$wait =  .2
-                etc[["todo"]]$unclick =  F
-            } else {
-                if(length(etc[["todo"]]$actions) > 0){
-
-                    act = etc[["todo"]]$actions[1]
-
-                    if(act %in% c("A","B","C")) {
-                        tama$SetButton(c(A = 0, B = 1, C = 2)[act],T)
-                        etc[["todo"]]$wait =  .1
-                        etc[["todo"]]$unclick =  T
-                    } else if(act == "AC") {
-                        for(b in c(0,2)) tama$SetButton(b,T)
-                        etc[["todo"]]$wait =  2
-                        etc[["todo"]]$unclick =  T
-                    } else {
-                        etc[["todo"]]$wait = as.numeric(act)
-                    }
-
-                    etc[["todo"]]$actions = etc[["todo"]]$actions[-1]
-                }
-            }
-        }
-    })
+        })
 
         ## Original gameplay
         observeEvent(input$A, {etc[["todo"]] = list(wait = 0, actions = "A",  unclick = F)})
